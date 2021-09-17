64eff4dc93bb3a1e70da401cb5fffd26bd75f542
js <grawires@gmail.com>
2021-09-17 20:28:56 +0200

    a

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -51,8 +51,8 @@ export function tokenGetter() {
     JwtModule.forRoot({
       config: {
         tokenGetter: () => { return localStorage.getItem("jwtToken"); },
-        allowedDomains: [ environment.API_URL+":4023" ],
-        disallowedRoutes: [ environment.API_URL + ":4023/auth/login"]
+        allowedDomains: [ environment.API_URL+":38904" ],
+        disallowedRoutes: [ environment.API_URL + ":38904/auth/login"]
       },
     }),
     
