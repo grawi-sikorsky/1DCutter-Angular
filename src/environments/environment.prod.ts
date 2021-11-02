6047c7af49c21d836068e5c77a00beec5b8164cb
js-job <grawires@gmail.com>
2021-11-02 20:23:09 +0100

    cordova

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-//  API_URL: 'https://onedcutter.herokuapp.com'
-  API_URL: 'http://10.0.2.2:8080'
+  API_URL: 'https://onedcutter.herokuapp.com'
+  //API_URL: 'http://10.0.2.2:8080'
 };
