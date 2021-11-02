a64189c1724a35bec752e639994969ca724dfb87
js-job <grawires@gmail.com>
2021-11-02 19:16:25 +0100

    run

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  API_URL: 'https://onedcutter.herokuapp.com'
+//  API_URL: 'https://onedcutter.herokuapp.com'
+  API_URL: 'http://10.0.2.2:8080'
 };
